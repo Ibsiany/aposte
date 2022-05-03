2d2680e794d0e1db94aa8ccd71648cc0d8482dda
ibsiany <ibsianyd@gmail.com>
2022-05-03 19:31:17 -0300

    inicializando autenticação

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,107 @@
+import {
+    createContext,
+    ReactNode,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from 'react';
+import { api } from '../services/api';
+  
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  token: string
+}
+
+interface SignInCredentials {
+  cpf: string;
+  code: string;
+}
+  
+interface AuthContextData {
+  user: User;
+  isAuthenticated: boolean;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+  
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+  
+  export function AuthProvider({ children }: AuthProviderProps) {
+    const [data, setData] = useState<User>(() => {
+      const user = localStorage.getItem('@Aposte:user');
+  
+      if (user) {
+        api.defaults.headers.common.Authorization = `Bearer ${
+          JSON.parse(user).token
+        }`;
+  
+        return JSON.parse(user);
+      }
+  
+      return {} as User;
+    });
+  
+    useEffect(() => {
+      const user = localStorage.getItem('@Aposte:user');
+  
+      if (user) {
+        setData(JSON.parse(user));
+  
+        api.defaults.headers.common.Authorization = `Bearer ${
+          JSON.parse(user).token
+        }`;
+      }
+    }, []);
+  
+    const isAuthenticated = !!data.token;
+  
+    const signIn = useCallback(async ({ cpf, code }: SignInCredentials) => {
+      try {
+        const response = await api.post('/session', { cpf, code });
+  
+        setData(response.data);
+  
+        localStorage.setItem(
+          '@Aposte:user',
+          JSON.stringify({
+            code: response.data.code,
+            token: response.data.token,
+          }),
+        );
+  
+        api.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+  
+    const signOut = useCallback(() => {
+      localStorage.removeItem('@Aposte:user');
+  
+      setData({} as User);
+    }, []);
+  
+    return (
+      <AuthContext.Provider
+        value={{ signIn, signOut, isAuthenticated, user: data }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
+  }
+  
+  export function useAuth() {
+    const context = useContext(AuthContext);
+  
+    return context;
+  }
+  
