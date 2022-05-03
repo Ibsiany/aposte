cb72066157d490645fd5d4ee35b1a0a0b100191e
ibsiany <ibsianyd@gmail.com>
2022-05-03 18:10:18 -0300

    adicionando valores

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,30 @@
+import { ChangeEvent, useCallback } from "react";
 import { Container, Label, InputForms } from "./styles";
 
 interface IInputProps {
     type: string;
     label: string;
+    value: string;
+    setValue: (value: string) => void;
 }
 
-export function Input({type, label}:IInputProps){
+export function Input({type, label, value, setValue}:IInputProps){
+    const handleInputChange = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => {
+          const { value } = event.target;
+    
+          setValue(value);
+        },
+        [setValue],
+      );
+
+
     return (
         <Container>
             <Label>
                 {label}:
             </Label>
-            <InputForms type={type}/>
+            <InputForms type={type} onChange={handleInputChange} value={value}/>
         </Container>
     )
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { Container, Title, LoginContainer,NewUser } from './styles'
 
 export function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <Container>
             <LoginContainer>
                 <Title>Login</Title>          
-                <Input type="text" label="E-mail" />
-                <Input type="password" label="Senha" />
+                <Input type="text" label="E-mail" value={email} setValue={setEmail}/>
+                <Input type="password" label="Senha" value={password} setValue={setPassword}/>
 
                 <Button name="Entrar"/>
 
